772421f71575ce6a2884cf9668e2ea9661208baa
IvaDeneva <156186907+IviAve@users.noreply.github.com>
2024-04-23 14:46:42 +0300

    electrificityBill

---
+++ tok/tok/Form1.Designer.cs
@@ -0,0 +1,427 @@
+﻿
+namespace tok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBoxSum1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxNight = new System.Windows.Forms.TextBox();
+            this.textBoxSum2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBoxDay = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.textBoxSum3 = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(682, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBoxSum1
+            // 
+            this.textBoxSum1.Location = new System.Drawing.Point(597, 68);
+            this.textBoxSum1.Name = "textBoxSum1";
+            this.textBoxSum1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum1.TabIndex = 4;
+            this.textBoxSum1.Text = "                       ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(313, 106);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBoxNight
+            // 
+            this.textBoxNight.Location = new System.Drawing.Point(439, 106);
+            this.textBoxNight.Name = "textBoxNight";
+            this.textBoxNight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNight.TabIndex = 7;
+            // 
+            // textBoxSum2
+            // 
+            this.textBoxSum2.Location = new System.Drawing.Point(597, 106);
+            this.textBoxSum2.Name = "textBoxSum2";
+            this.textBoxSum2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum2.TabIndex = 8;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(497, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(559, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(419, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "=";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(313, 68);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 14;
+            // 
+            // textBoxDay
+            // 
+            this.textBoxDay.Location = new System.Drawing.Point(439, 68);
+            this.textBoxDay.Name = "textBoxDay";
+            this.textBoxDay.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDay.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(153, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(153, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(82, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "OLD";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(210, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "NEW";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 71);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "DAY";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 113);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "NIGHT";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(170, 106);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(289, 75);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "=";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(289, 113);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "=";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(368, 41);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(30, 13);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "DIFF";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(481, 41);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(76, 13);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "CONSTPRICE";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(618, 41);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(50, 13);
+            this.label15.TabIndex = 29;
+            this.label15.Text = "RESULT";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(500, 155);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(72, 13);
+            this.label16.TabIndex = 30;
+            this.label16.Text = "TOTAL_SUM";
+            // 
+            // textBoxSum3
+            // 
+            this.textBoxSum3.Location = new System.Drawing.Point(597, 152);
+            this.textBoxSum3.Name = "textBoxSum3";
+            this.textBoxSum3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSum3.TabIndex = 31;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(642, 136);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(13, 13);
+            this.label17.TabIndex = 32;
+            this.label17.Text = "=";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(72, 268);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 34;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(597, 268);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 35;
+            this.loadButton.Text = "LOAD";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(28, 12);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(119, 20);
+            this.textBoxDate.TabIndex = 36;
+            this.textBoxDate.TextChanged += new System.EventHandler(this.textBoxDate_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(717, 303);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.textBoxSum3);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDay);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxSum2);
+            this.Controls.Add(this.textBoxNight);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBoxSum1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "EnergoProSmetka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBoxSum1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBoxNight;
+        private System.Windows.Forms.TextBox textBoxSum2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBoxDay;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox textBoxSum3;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox textBoxDate;
+    }
+}
+
---
+++ tok/tok/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // const double DAY_OLD = 20441;
+            // const double NIGHT_OLD = 8652;
+
+            // const double DAY_PRICE = 0.245;
+            // const double NIGHT_PRICE = 0.137;
+
+            double dayOld = double.Parse(this.textBox1.Text);
+            double dayNew = double.Parse(this.textBox2.Text);
+            double nightOld = double.Parse(this.textBox3.Text);
+            double nightNew = double.Parse(this.textBox4.Text);
+            double dayPrice = double.Parse(this.textBoxDay.Text);
+            double nightPrice = double.Parse(this.textBoxNight.Text);
+            string date = (this.textBoxDate.Text);
+
+            double diffDay = dayNew - dayOld;
+            double diffNight = nightNew - nightOld;
+
+            double dayResult = diffDay * dayPrice;
+            double nightResult = diffNight * nightPrice;
+            double totalSum = dayResult + nightResult;
+
+           // textBox1.Text = dayOld.ToString();
+           // textBox3.Text = dayNew.ToString();
+
+            textBox9.Text = diffDay.ToString();
+            textBox6.Text = diffNight.ToString();
+
+           
+            textBoxDay.Text = dayPrice.ToString();
+            textBoxNight.Text = nightPrice.ToString();
+
+            textBoxSum1.Text = dayResult.ToString();
+            textBoxSum2.Text = nightResult.ToString();
+            textBoxSum3.Text = totalSum.ToString();
+            textBoxDate.Text = date.ToString();
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Settings2.Default.dayOld = textBox1.Text;
+            Settings2.Default.nightOld = textBox3.Text;
+            Settings2.Default.dayNew = textBox2.Text;
+            Settings2.Default.nightNew = textBox4.Text;
+            Settings2.Default.diffDay = textBox9.Text;
+            Settings2.Default.diffNight = textBox6.Text;
+            Settings2.Default.dayPrice = textBoxDay.Text;
+            Settings2.Default.nightPrice = textBoxNight.Text;
+            Settings2.Default.dayResult = textBoxSum1.Text;
+            Settings2.Default.nightResult = textBoxSum2.Text;
+            Settings2.Default.totalSum = textBoxSum3.Text;
+            Settings2.Default.date = textBoxDate.Text;
+            
+            Settings2.Default.Save();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LOAD();
+        }
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            LOAD();
+        }
+        void LOAD()
+        {
+            textBox1.Text = Settings2.Default.dayOld;
+            textBox3.Text = Settings2.Default.nightOld;
+            textBox2.Text = Settings2.Default.dayNew;
+            textBox4.Text = Settings2.Default.nightNew;
+            textBox9.Text = Settings2.Default.diffDay;
+            textBox6.Text = Settings2.Default.diffNight;
+            textBoxDay.Text = Settings2.Default.dayPrice;
+            textBoxNight.Text = Settings2.Default.nightPrice;
+            textBoxSum1.Text = Settings2.Default.dayResult;
+            textBoxSum2.Text = Settings2.Default.nightResult;
+            textBoxSum3.Text = Settings2.Default.totalSum;
+            textBoxDate.Text = Settings2.Default.date;
+        }
+
+        private void textBoxDate_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+//private void loadButton_Click(object sender, EventArgs e)
+//{
+//    LOAD();
+//}
+//void LOAD()
+//{
+//    textBox1.Text = Settings2.Default.dayOld;
+//    textBox3.Text = Settings2.Default.nightOld;
+//    textBox2.Text = Settings2.Default.dayNew;
+//    textBox4.Text = Settings2.Default.nightNew;
+//    textBox9.Text = Settings2.Default.diffDay;
+//    textBox6.Text = Settings2.Default.diffNight;
+//    textBoxDay.Text = Settings2.Default.dayPrice;
+//    textBoxNight.Text = Settings2.Default.nightPrice;
+//    textBoxSum1.Text = Settings2.Default.dayResult;
+//    textBoxSum2.Text = Settings2.Default.nightResult;
+//    textBoxSum3.Text = Settings2.Default.totalSum;
+//}
